ecb4c3e5d5c518cefa34183591b0b793106747c7
Alexander <7jobbox7@mail.ru>
2022-12-30 19:43:29 +0300

    555

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -20,9 +20,9 @@ export const Registration = () => {
     formState: { errors, isValid },
   } = useForm({
     defaultValues: {
-      fullName: 'Петя губкин',
-      email: 'hui@hui.ru',
-      password: '1234',
+      fullName: '',
+      email: '',
+      password: '',
     },
 
     mode: 'onChange',
